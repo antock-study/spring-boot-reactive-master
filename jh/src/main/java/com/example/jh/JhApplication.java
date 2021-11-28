296c1d6711d583537d2a9598f0e6a80fc2d314d7
zhenxikim <8055kjh@gmail.com>
2021-11-28 22:29:08 +0900

    6장 스프링 API 서버 구축

--- jh/src/main/java/com/example/jh/JhApplication.java
+++ jh/src/main/java/com/example/jh/JhApplication.java
@@ -1,13 +1,12 @@
-package com.example.demo;
+package com.example.jh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JhApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JhApplication.class, args);
 	}
-
 }
